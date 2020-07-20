41fa1956c47d144f6daa6232d813afd856489c4d
Sebastian Silva <seebassilva@gmail.com>
2020-07-19 22:25:16 -0500

    Create user viewset

---
+++ api/api/woke/serializers.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    password = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password', )
+
+    def create(self, validated_data):
+        if not self.context.get('request').user.is_superuser:
+            raise serializers.ValidationError("No tienes permisos para realizar esta acción")
+        
+        user, created = User.objects.get_or_create(
+                username=validated_data['email'],
+                email=validated_data['email'],
+                first_name=validated_data['first_name'],
+                last_name=validated_data['last_name'],
+            )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return super(UserSerializer, self).create(validated_data)
