ec5549d3e81f7639d46a081c15bf14474b68181b
Sebastian Silva <seebassilva@gmail.com>
2020-07-19 22:25:27 -0500

    include routes

--- api/api/woke/urls.py
+++ api/api/woke/urls.py
@@ -1,10 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
 from woke import views 
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'user', views.UserViewset, basename='userviewset')
+
 
 urlpatterns = [
     # Your URLs...
-    path('user/', UserView),
+    path(r'', include(router.urls)),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api-auth/', include('rest_framework.urls')),
+
 ]
