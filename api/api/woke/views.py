955cc5758624c67482ce965203964b3525bcfd06
Sebastian Silva <seebassilva@gmail.com>
2020-07-19 22:25:37 -0500

    Angular initial commit

--- api/api/woke/views.py
+++ api/api/woke/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from django.contrib.auth.models import User
+from woke import serializers
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
  
 
 class UserViewset(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = serializers.UserSerializer
+    permission_classes = (IsAdminUser, IsAuthenticated,)
+    
+    def get_serializer_context(self):
+        return {'request': self.request}
