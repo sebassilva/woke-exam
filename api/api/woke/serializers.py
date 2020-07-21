816de0c36d2b45632fec282baaf92deb8ad0f819
Sebastian Silva <seebassilva@gmail.com>
2020-07-20 23:32:09 -0500

    add issuperuser attribute

--- api/api/woke/serializers.py
+++ api/api/woke/serializers.py
@@ -7,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password', )
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password', 'is_superuser')
 
     def create(self, validated_data):
         if not self.context.get('request').user.is_superuser:
