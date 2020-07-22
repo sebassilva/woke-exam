91846c4139ecae21f3fbf94d027b1a40264adc9c
Sebastian Silva <seebassilva@gmail.com>
2020-07-21 20:20:57 -0500

    create all services

---
+++ dashboard/woke/src/app/api.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private BASE_URL: string = 'http://localhost:8000/api';
+  private httpHeaders: HttpHeaders = new HttpHeaders()
+  .set('Content-Type', 'application/json');
+
+  private options = {
+      headers: this.httpHeaders,
+      responseType: 'json'
+    };
+
+
+    constructor(private http: HttpClient) {
+    }
+
+    getAuthHeaders(){
+      console.log(localStorage.getItem('user'))
+      const accessToken = JSON.parse(localStorage.getItem('user')).access;
+      console.log("Acces token", accessToken);
+      const httpHeaders = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${accessToken}`);
+      const options = {
+            headers: httpHeaders,
+            responseType: 'json',
+          };
+      return options;
+    }
+
+
+    login(data): Observable<any> {
+      const url = `${this.BASE_URL}/token/`;
+      return this.http.post(url, data, this.options as any);
+    }
+
+
+    getUsers(): Observable<any> {
+      const url = `${this.BASE_URL}/user/`;
+      return this.http.get(url, this.getAuthHeaders() as any);
+    }
+
+    getUserDetail(id): Observable<any> {
+      const url = `${this.BASE_URL}/user/${id}/`;
+      return this.http.get(url, this.getAuthHeaders() as any);
+    }
+
+    postUserDetail(user): Observable<any> {
+      const url = `${this.BASE_URL}/user/`;
+      return this.http.post(url, user, this.getAuthHeaders() as any);
+    }
+
+    putUserDetail(id, user): Observable<any> {
+      const url = `${this.BASE_URL}/user/${id}/`;
+      return this.http.put(url, user, this.getAuthHeaders() as any);
+    }
+
+    deleteUserDetail(id: number): Observable<any> {
+      const url = `${this.BASE_URL}/user/${id}/`;
+      return this.http.delete(url, this.getAuthHeaders() as any);
+    }
+}
