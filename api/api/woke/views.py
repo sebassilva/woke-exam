b3c023c13d9a5fc94f03a88720bc96d3d375d056
Sebastian Silva <seebassilva@gmail.com>
2020-07-21 20:21:30 -0500

    Use IsAdminuserorReadonly

--- api/api/woke/views.py
+++ api/api/woke/views.py
@@ -3,12 +3,14 @@ from rest_framework import viewsets, permissions
 from django.contrib.auth.models import User
 from woke import serializers
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from woke.permissions import IsAdminUserOrReadOnly
  
 
 class UserViewset(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
-    permission_classes = (IsAdminUser, IsAuthenticated,)
+    permission_classes = (IsAdminUserOrReadOnly,)
     
     def get_serializer_context(self):
         return {'request': self.request}
+    
