a2841338868366a853ae4510c5c8a505d44889e2
Sebastian Silva <seebassilva@gmail.com>
2020-07-21 20:21:13 -0500

    add complementary routing

--- dashboard/woke/src/app/app-routing.module.ts
+++ dashboard/woke/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { UserComponent } from "./user/user.component";
+import { UserDetailComponent } from "./user-detail/user-detail.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'user-detail', component: UserDetailComponent },
+  { path: 'user-detail/:id', component: UserDetailComponent },
+
+  // Redirect non existing to login
+  {path:'**',redirectTo:'login', pathMatch: 'full' },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
