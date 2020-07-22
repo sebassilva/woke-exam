61048b23a7285f3a6055dd8b56de135d86056b5e
Sebastian Silva <seebassilva@gmail.com>
2020-07-21 20:19:24 -0500

    Create and edit user

--- dashboard/woke/src/app/user-detail/user-detail.component.ts
+++ dashboard/woke/src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from '../api.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,10 +10,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.scss']
 })
 export class UserDetailComponent implements OnInit {
+  userForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private api: ApiService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.userForm = this.formBuilder.group({
+      first_name: '',
+      last_name: '',
+      username: '',
+      password: ''
+    });
+   }
 
   ngOnInit(): void {
+    this.route.params.subscribe( params => {
+      const id = params.id;
+      this.api.getUserDetail(id)
+        .subscribe( data => {
+          this.userForm.patchValue(data);
+        })
+    });
   }
 
+
+  onSubmit(): void{
+    this.api.postUserDetail(this.userForm.value).subscribe( data => {
+      if ( data ){
+        // If login is correct, navigate to users
+        Swal.fire(
+          '¡Usuario creado!',
+          'Regresaremos a la lista de usuarios..',
+          'success'
+        );
+        this.router.navigate(['/user']);
+      }
+    }, error => {
+      console.log(error)
+      Swal.fire(
+        '¡Hubo un problema!',
+        error.error.detail,
+        'error'
+      );
+   });
+  }
 }
