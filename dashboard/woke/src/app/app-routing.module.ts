3f86a6a28d7a0a7b3d74ac02f9625dcb6316529c
Sebastián Silva <seebassilva@gmail.com>
2020-07-29 16:41:50 -0500

    created guard to protect views

--- dashboard/woke/src/app/app-routing.module.ts
+++ dashboard/woke/src/app/app-routing.module.ts
@@ -3,19 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { UserComponent } from "./user/user.component";
 import { UserDetailComponent } from "./user-detail/user-detail.component";
+import { AuthGuard } from './guards/auth-guard.service';
+
 
 
 const routes: Routes = [
-  {path:'',redirectTo:'login', pathMatch: 'full' },
+  {path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'user', component: UserComponent },
-  { path: 'user-detail', component: UserDetailComponent },
-  { path: 'user-detail/:id', component: UserDetailComponent },
+  { path: 'user', component: UserComponent, canActivate: [AuthGuard] },
+  { path: 'user-detail', component: UserDetailComponent, canActivate: [AuthGuard] },
+  { path: 'user-detail/:id', component: UserDetailComponent, canActivate: [AuthGuard] },
 
   // Redirect non existing to login
-  {path:'**',redirectTo:'login', pathMatch: 'full' },
-
-
+  {path: '**', redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
