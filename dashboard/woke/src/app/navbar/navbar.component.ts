c2a79a74991be7148cec8afa7814241da7f7cb74
Sebastián Silva <seebassilva@gmail.com>
2020-07-29 16:40:56 -0500

    Fixed responsive navbar

--- dashboard/woke/src/app/navbar/navbar.component.ts
+++ dashboard/woke/src/app/navbar/navbar.component.ts
@@ -9,6 +9,8 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
 
+  public navbarCollapsed = true;
+
   constructor(
     private route: Router
   ) { }
